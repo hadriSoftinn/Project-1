05c7ccb285068167db29a4d0f3b670f6fc6b8b95
Izz Hadri <hadri@mysoftinn.com>
2022-03-20 22:46:18 +0800

    using webpack compiler for a single file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 // Hello Comment
-const message: string = "Hello World";
+// const Main = require("./app/Main");
+import { Main } from "./app/Main";
+
+const message: string = "Hello World!!";
 console.log(message);
+
+// new Main.render();
